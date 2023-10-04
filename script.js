4820efb2789c80359f4f3e02b2d60e5926ee0508
nelsonSauer <sauer@senai>
2023-10-04 16:35:53 -0300

    feat: cálculo da média.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-console.log("Olá, console!");
+// Lógica de Programação
+// Problema : Obter 3 notas de um aluno, calcular a média.
+// exibir a média e exibir a situação do aluno: aprovado,
+// em exame ou reprovado,
 
-document.write("Olá, documento!");
+console.log('Inicio do programa');
 
-window.alert("Olá, Janela!");
+//declaração das variaveis:
+// Variavel constante
+// parseInt serve para converter caracteres em numeros. 
+const nota1 = parseInt(prompt('Digite a nota 1:'));
+const nota2 = parseInt(prompt('Digite a nota 2:'));
+const nota3 = parseInt(prompt('Digite a nota 3:'));
+
+
+// cálculo da média 
+const media= (nota1 + nota2 + nota3) / 3;
+
+console.log('A nota 1 é:', nota1);
+console.log('A nota 2 é:', nota2);
+console.log('A nota 3 é:', nota3);
+console.log('A média do aluno é:' , media);
+
+
+console.log('Fim do programa');
